0ea4a35dc572d636013a2e1c1e84ed3fd0aa4006
simjxu <simjxu@gmail.com>
2018-10-14 23:10:17 -0700

    test

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,8 +2,8 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/home/simon/Projects/Github-simjxu/simjxu.github.io/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
   "component---src-templates-blog-post-js": () => import("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
+  "component---cache-dev-404-page-js": () => import("/home/simon/Projects/Github-simjxu/simjxu.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-contact-js": () => import("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,8 +3,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/home/simon/Projects/Github-simjxu/simjxu.github.io/node_modules/gatsby-plugin-offline/app-shell.js")),
   "component---src-templates-blog-post-js": preferDefault(require("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/templates/blog-post.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/home/simon/Projects/Github-simjxu/simjxu.github.io/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/pages/about.js")),
   "component---src-pages-contact-js": preferDefault(require("/home/simon/Projects/Github-simjxu/simjxu.github.io/src/pages/contact.js")),
